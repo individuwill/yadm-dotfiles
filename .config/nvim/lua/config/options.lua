ddab93565560a865df6df627eafb9d6989699c4e
Will Smith <wsmith85@gmail.com>
2024-10-27 19:57:53 -0700

    configure to use dedicated python venv

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Will need to setup a dedicated python environment for neovim
+--
+-- uv venv .local/share/nvim_python
+-- . ~/.local/share/nvim_python/bin/activate
+-- uv pip install pynvim
+vim.g.python3_host_prog = "~/.local/share/nvim_python/bin/python3"
