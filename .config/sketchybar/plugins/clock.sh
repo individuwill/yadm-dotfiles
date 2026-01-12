1c1f08819eb12bb9bbc79e872ce6a14bf846fe00
Will Smith <wsmith85@gmail.com>
2026-01-12 08:58:42 -0800

    add timezones to time widget

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
+# Usage:
+#   clock.sh                -> local time
+#   clock.sh UTC            -> UTC time
+#   clock.sh America/New_York
+#   clock.sh Asia/Tokyo
+#   clock.sh Europe/London
+
 # The $NAME variable is passed from sketchybar and holds the name of
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%m/%d %H:%M')"
+zone="${1-}"
 
+format="+%m/%d %H:%M"
+# Optional: show zone abbreviation when a zone is specified
+[ -n "$zone" ] && format="$format %Z"
+
+if [ -n "$zone" ]; then
+	label="$(TZ="$zone" date "$format")"
+else
+	label="$(date "$format")"
+fi
+
+sketchybar --set "$NAME" label="$label"
